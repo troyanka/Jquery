a4a620c840ef7332c84009d9874c04c02d549bf2
troyanka <troyanka@gmail.com>
2018-12-31 12:41:08 +0200

    update

--- userReviews/scripts.js
+++ userReviews/scripts.js
@@ -1,30 +1,79 @@
-$(document).ready(function(){
-   init();
+$(document).ready(function () {
+    let comments = localStorage.getItem('comments') ? JSON.parse(localStorage.getItem('comments')) : [];
 
-    //Add comment 
-   $('.add-comment').click(addComment);
+    init();
+
+    //Event Listeners 
+    // $('.add-comment').click(addComment);
+    $('main').on('click', '.add-comment', addComment);
+    $('.user-comments').on('click', 'i.fa-trash-alt', deleteComment);
+    $('.user-comments').on('click', 'i.fa-pencil-alt', editComment);
+
+    function init() {
+        if (comments) {
+            comments.forEach(comment => addCommentToDOM(comment));
+        }
+    }
+
+    function editComment(e){
+        let closestId = $(e.target).closest('div.comment').data('id'); 
+        console.log("closestId", closestId); 
+        $('.form-for-comment').hide();
+
+        let myComment = comments.filter(function(comment){
+            return comment.id === closestId;
+        });
+        myComment = myComment[0];
+        console.log("myComment", myComment);
+        let updateForm = `<div class="form-for-comment">
+            <input class="user-name" type="text" name="user-name" placeholder="Your name" value="${myComment.Fname} ${myComment.Lname}"/>
+            <textarea name="user-comment" id="user-comment" cols="30" placeholder="Your comment">${myComment.comment}</textarea>
+            <button class="add-comment">Add</button>
+        </div>`;
+        let $editedElem = $(`div[data-id=${closestId}]`);
+        $(updateForm).insertBefore($editedElem);
+        $editedElem.hide();
+        //$("main").prepend(updateForm);
+    }
+
+    function deleteComment(e){
+        let closestId = $(e.target).closest('div.comment').data('id'); 
+        comments = comments.filter(function(comment){
+            return comment.id != closestId;
+        });
+        localStorage.setItem("comments", JSON.stringify(comments));
+        $(`div[data-id=${closestId}]`).remove();
+    }
+
+    function addComment() {
+        let fullname = $('.form-for-comment input[name="user-name"]').val().split(' ');
+        let commentDetails = {
+            Fname: fullname[0],
+            Lname: fullname[1],
+            comment: $('.form-for-comment #user-comment').val(),
+            id: new Date().getTime()
+        }
+        comments.push(commentDetails);
+        localStorage.setItem("comments", JSON.stringify(comments));
+        addCommentToDOM(commentDetails);
+    }
+
+    function addCommentToDOM(commentDetails) {
+        let commenthtml = `<div class="comment" data-id=${commentDetails.id}>
+        <img src="https://ui-avatars.com/api/?name=${commentDetails.Fname}+${commentDetails.Lname}"/>
+        <h3>${commentDetails.Fname} ${commentDetails.Lname}</h3>
+        <p>${commentDetails.comment}</p>
+        <div class="icons">
+                <i class="fas fa-pencil-alt"></i>
+                <i class="fas fa-trash-alt"></i>
+        </div>
+        </div>`
+    
+        $('.user-comments').append(commenthtml);
+    }
 });
 
-let comments = localStorage.getItem('comments') ? JSON.parse(localStorage.getItem('comments')) : [];
 
-function init(){
-    console.log("comments", comments);
-    if(comments){
-        comments.map(comment=>{
-            return console.log(comment.name);
-        });
-    }
-}
 
-function addComment(){
-    console.log('Add comment clicked');
-    let commentDetails = {
-        name: $('.form-for-comment input[name="user-name"]').val(),
-        comment: $('.form-for-comment #user-comment').val()
-    }
-    comments.push(commentDetails);
 
-    localStorage.setItem("comments", JSON.stringify(comments));
 
-    console.log(JSON.stringify(comments));
-}
